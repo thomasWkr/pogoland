a1c5e5ecbaccfaab273c917f8072b058b548f955
Kenjikuta <kenjikuta@gmail.com>
2024-10-19 11:35:25 -0300

    persue added

--- Scenes/Characters/bird.gd
+++ Scenes/Characters/bird.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 const SPEED = 50.0
 var pogoed = false
+var persue = false
+
+# Reference to the player
+var player: Node2D = null
 
 func _physics_process(delta: float) -> void:
 	if(pogoed):
@@ -9,6 +13,11 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = -SPEED
 
+	if(persue):
+		velocity.x = -SPEED
+	else: 
+		velocity.x = 0
+		
 	move_and_slide()
 	
 func _on_pogo_detection_area_body_entered(body: Node2D) -> void:
@@ -20,4 +29,16 @@ func _on_pogo_detection_area_body_entered(body: Node2D) -> void:
 func _on_pogo_detection_area_body_exited(body: Node2D) -> void:
 	if(body.name == "Pc"):
 		body.can_pogo = false
+	pass # Replace with function body.
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if(body.name == "Pc"):
+		persue = true
+	pass # Replace with function body.
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	if(body.name == "Pc"):
+		persue = false
 	pass # Replace with function body.
