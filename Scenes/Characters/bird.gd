c957d676137ee2c7b90f3331ca5965abe4adaa89
DiegoHommer <daugsteel@gmail.com>
2024-10-19 02:18:25 -0300

    working pogo

--- Scenes/Characters/bird.gd
+++ Scenes/Characters/bird.gd
@@ -1,8 +1,19 @@
-extends Area2D
+extends CharacterBody2D
 
+const SPEED = 50.0
 
+func _physics_process(delta: float) -> void:
+	velocity.x = -SPEED
 
-func _on_body_entered(body: Node2D) -> void:
+	move_and_slide()
+	
+func _on_pogo_detection_area_body_entered(body: Node2D) -> void:
 	if(body.name == "Pc"):
 		body.can_pogo = true
+	pass # Replace with function body.
+
+
+func _on_pogo_detection_area_body_exited(body: Node2D) -> void:
+	if(body.name == "Pc"):
+		body.can_pogo = false
 	pass # Replace with function body.
