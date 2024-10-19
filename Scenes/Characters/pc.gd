f3c4e6181359fb858ddd391204cc75fb059603fe
Thomas Wiederkehr <thomas-611@hotmail.com>
2024-10-19 10:04:20 -0300

    Player respawn

--- Scenes/Characters/pc.gd
+++ Scenes/Characters/pc.gd
@@ -2,12 +2,17 @@ extends CharacterBody2D
 
 @onready var camera = %Camera2D
 
+var initial_position = 0
 var can_pogo = false
 var got_hit = false
 var bird = ''
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
+
+func _init() -> void:
+	initial_position = global_position
 
 func _physics_process(delta: float) -> void:
 	if(got_hit):
@@ -33,12 +38,5 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func death() -> void:
-	# Detach the camera by reparenting it to the scene root
-	var current_camera_position = camera.global_transform
-	camera.get_parent().remove_child(camera)
-	get_tree().root.add_child(camera) 
-	camera.global_transform = current_camera_position 
-	
-	# Remove PC node
-	queue_free()
-	
+	# Respawn the player to the original position
+	global_position = initial_position
