1eb860eb1d814b35e38455c75263f7f1efb63ca0
Thomas Wiederkehr <thomas-611@hotmail.com>
2024-10-20 14:13:10 -0300

    Revert "Merge branch 'main' into spike_sprites"
    This reverts commit fbb1146cbe5e4857e7e73b44bf7a107e883251f0, reversing
    changes made to 9423ec0f5285f3a422d51538de6f717372a2afcb.

--- Scenes/Characters/pc.gd
+++ Scenes/Characters/pc.gd
@@ -14,6 +14,8 @@ extends CharacterBody2D
 @onready var particle_emitter = %BLOODSPREAD
 @onready var pogo_emitter = %PogoSpread
 
+
+
 var initial_position = 0
 var collectable_count = 0
 var is_jumping = true
@@ -21,7 +23,6 @@ var can_pogo = false
 var got_hit = false
 var alive = true
 var bird = ''
-var can_tp = 1
 
 # Screenshake variables
 var shake_magnitude = 10  # Default shake intensity
@@ -29,10 +30,10 @@ var shake_duration = 0.1  # Default shake duration
 var shake_timer = 0.0     # Timer to track the shake duration
 var original_camera_position = Vector2()
 
-const DELTA = 10
-var SPEED = 300.0
+
+const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
-var GRAVITY = 1100.0
+const GRAVITY = 1100.0
 const ASCEND_MULTIPLIER = 1.6  # Controls the ascend speed (higher = faster)
 const DESCEND_MULTIPLIER = 1 # Controls the descend speed (lower = slower)
 const PARALLAXES_TOTAL = 4
@@ -44,95 +45,6 @@ const SEAGULL_AUDIO = preload("res://Assets/Audio/seagull.ogg")
 const UFO_AUDIO = preload("res://Assets/Audio/ufo.ogg")
 const FAIRY_AUDIO = preload("res://Assets/Audio/fairy.ogg")
 
-@onready var tilemap_layer1 = get_parent().get_child(3).get_child(1)
-@onready var tilemap_layer2 = get_parent().get_child(1).get_child(0).get_child(0).get_child(2)
-@onready var tilemap_layer3 = get_parent().get_child(1).get_child(1).get_child(0).get_child(2)
-@onready var tilemap_layer4 = get_parent().get_child(1).get_child(2).get_child(0).get_child(2)
-@onready var tilemap_layer5 = get_parent().get_child(2).get_child(0).get_child(0).get_child(2)
-
-var current_layer = 1
-var changing_layer = false
-
-func up_layer():
-	if current_layer == 5:
-		current_layer= 1
-	else: 
-		current_layer+=1
-
-func down_layer():
-	if current_layer == 1:
-		current_layer= 5
-	else: 
-		current_layer-=1
-
-
-#shader function
-func flash():
-	print(current_layer)
-	tilemap_layer1.material.set_shader_parameter("modifier", 0.5)
-	tilemap_layer2.material.set_shader_parameter("modifier", 0.5)
-	tilemap_layer3.material.set_shader_parameter("modifier", 0.65)
-	tilemap_layer4.material.set_shader_parameter("modifier", 0.8)
-	tilemap_layer5.material.set_shader_parameter("modifier", 0.5)
-
-	
-func unflash(current_layer):
-	match current_layer:
-		1:
-			tilemap_layer1.material.set_shader_parameter("modifier", 0)
-			tilemap_layer2.material.set_shader_parameter("modifier", 0.5)
-			tilemap_layer3.material.set_shader_parameter("modifier", 0.65)
-			tilemap_layer4.material.set_shader_parameter("modifier", 0.8)
-			tilemap_layer5.material.set_shader_parameter("modifier", 0.5)
-		2:
-			tilemap_layer1.material.set_shader_parameter("modifier", 0.5)
-			tilemap_layer2.material.set_shader_parameter("modifier", 0)
-			tilemap_layer3.material.set_shader_parameter("modifier", 0.5)
-			tilemap_layer4.material.set_shader_parameter("modifier", 0.65)
-			tilemap_layer5.material.set_shader_parameter("modifier", 0.8)
-		3:
-			tilemap_layer1.material.set_shader_parameter("modifier", 0.8)
-			tilemap_layer2.material.set_shader_parameter("modifier", 0.5)
-			tilemap_layer3.material.set_shader_parameter("modifier", 0)
-			tilemap_layer4.material.set_shader_parameter("modifier", 0.5)
-			tilemap_layer5.material.set_shader_parameter("modifier", 0.65)
-		4:
-			tilemap_layer1.material.set_shader_parameter("modifier", 0.65)
-			tilemap_layer2.material.set_shader_parameter("modifier", 0.8)
-			tilemap_layer3.material.set_shader_parameter("modifier", 0.5)
-			tilemap_layer4.material.set_shader_parameter("modifier", 0)
-			tilemap_layer5.material.set_shader_parameter("modifier", 0.5)
-		5:
-			tilemap_layer1.material.set_shader_parameter("modifier", 0.5)
-			tilemap_layer2.material.set_shader_parameter("modifier", 0.65)
-			tilemap_layer3.material.set_shader_parameter("modifier", 0.8)
-			tilemap_layer4.material.set_shader_parameter("modifier", 0.5)
-			tilemap_layer5.material.set_shader_parameter("modifier", 0)
-
-# Variables to control the shader modifier
-var modifier := 0.0
-var modifier_speed := 3.0  # Speed of the modifier change
-var increasing := true  # Whether the modifier is increasing
-
-# Reference to the material (ensure you assign it in the inspector or via code)
-func shader_trans(delta: float) -> void:
-	if increasing:
-		modifier += modifier_speed * delta  # Increase the modifier
-		if modifier >= 1.0:
-			modifier = 1.0
-			increasing = false  # Start decreasing
-	else:
-		modifier -= modifier_speed * delta /1.5 # Decrease the modifier
-		if modifier <= 0.0:
-			modifier = 0.0
-			increasing = true  # Start increasing
-			changing_layer = false
-	# Apply the updated modifier to the shader
-	tilemap_layer1.material.set_shader_parameter("modifier", modifier)
-	tilemap_layer2.material.set_shader_parameter("modifier", modifier)
-	tilemap_layer3.material.set_shader_parameter("modifier", modifier)
-	tilemap_layer4.material.set_shader_parameter("modifier", modifier)
-	tilemap_layer5.material.set_shader_parameter("modifier", modifier)
 
 # Function to start the screenshake effect with custom intensity and duration
 func start_screenshake(intensity: float, duration: float) -> void:
@@ -148,22 +60,12 @@ func _process(delta: float) -> void:
 		
 		if shake_timer <= 0:
 			camera.position = original_camera_position  # Reset camera position after shaking
-	if changing_layer:
-		shader_trans(delta)
 
-func _iwadanit() -> void:
+func _init() -> void:
 	initial_position = global_position
 
 func _ready() -> void:
 	animation.play()
-	#flash()
-	#  unflash(current_layer)
-	# Imprimir o nome do nó que foi acessado
-	print(tilemap_layer1.name)
-	print(tilemap_layer2.name)
-	print(tilemap_layer3.name)
-	print(tilemap_layer4.name)
-	print(tilemap_layer5.name)
 
 func _physics_process(delta: float) -> void:
 	if(alive):
@@ -212,121 +114,60 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 
 func drop_player() -> void:
-	if (can_tp):
-		can_tp = 0
-		
-		changing_layer = true
-		#down_layer()
-		#flash()
-		#unflash(current_layer)
+	var layer1 = main_scene.get_child(3)
+	var layer2 = parallax_layer.get_child(0)
+	var layer3 = parallax_layer2.get_child(0)
+	var layer4 = parallax_layer3.get_child(0)
+	var layer5 = parallax_layer4.get_child(0)
 	
-		var layer1 = main_scene.get_child(3)
-		var layer2 = parallax_layer.get_child(0)
-		var layer3 = parallax_layer2.get_child(0)
-		var layer4 = parallax_layer3.get_child(0)
-		var layer5 = parallax_layer4.get_child(0)
-  
-		if is_on_floor():
-			velocity.y = -100
-		set_collision_layer_value(1,0)
-		set_collision_mask_value(1,0)
-			
-		animation.animation = "jumping"
-		is_jumping = true
-		for i in DELTA:
-			animation.scale+= Vector2(0.3, 0.3)
-			await get_tree().create_timer(0.01).timeout
-		
-		var buffer_position_1 = layer1.global_position
-		
-		layer1.global_position = layer2.global_position
-		layer1.scale = layer2.scale
-		layer2.global_position = layer3.global_position
-		layer2.scale = layer3.scale
-		layer3.global_position = layer4.global_position
-		layer3.scale = layer4.scale
-		layer4.global_position = layer5.global_position
-		layer4.scale = layer5.scale
-		
-		layer5.global_position = buffer_position_1
-		layer5.scale = Vector2(1,1)
-		
-		layer5.reparent(main_scene)
-		layer4.reparent(parallax_layer4)
-		layer3.reparent(parallax_layer3)
-		layer2.reparent(parallax_layer2)
-		layer1.reparent(parallax_layer)
-		
-		set_collision_layer_value(1,1)
-		set_collision_mask_value(1,1)
-		global_position.y-=100
-		
-		for i in DELTA:
-			animation.scale-= Vector2(0.3, 0.3)
-			await get_tree().create_timer(0.01).timeout
-		
-		await get_tree().create_timer(0.1).timeout
-		if is_on_floor():
-			animation.animation = "default" 
-		can_tp = 1
+	var buffer_position_1 = layer1.global_position
+	
+	layer1.global_position = layer2.global_position
+	layer1.scale = layer2.scale
+	layer2.global_position = layer3.global_position
+	layer2.scale = layer3.scale
+	layer3.global_position = layer4.global_position
+	layer3.scale = layer4.scale
+	layer4.global_position = layer5.global_position
+	layer4.scale = layer5.scale
+	
+	layer5.global_position = buffer_position_1
+	layer5.scale = Vector2(1,1)
+	
+	layer5.reparent(main_scene)
+	layer4.reparent(parallax_layer4)
+	layer3.reparent(parallax_layer3)
+	layer2.reparent(parallax_layer2)
+	layer1.reparent(parallax_layer)
+
 
 func lift_player() -> void:
-	if can_tp:
-		can_tp=0
-		changing_layer = true
-		#up_layer()
-		#flash()
-		#unflash(current_layer)
-		var layer1 = main_scene.get_child(3)
-		var layer2 = parallax_layer.get_child(0)
-		var layer3 = parallax_layer2.get_child(0)
-		var layer4 = parallax_layer3.get_child(0)
-		var layer5 = parallax_layer4.get_child(0)
-
-		var buffer_position_4 = layer4.global_position
-		var buffer_scale_4 = layer4.scale
-		
-		if is_on_floor():
-			velocity.y = -300
-		set_collision_layer_value(1,0)
-		set_collision_mask_value(1,0)
-			
-		animation.animation = "jumping"
-		is_jumping = true
-		for i in DELTA:
-			animation.scale-= Vector2(0.15, 0.15)
-			await get_tree().create_timer(0.01).timeout
-		
-		layer4.global_position = layer3.global_position
-		layer4.scale = layer3.scale
-		layer3.global_position = layer2.global_position
-		layer3.scale = layer2.scale
-		layer2.global_position = layer1.global_position
-		layer2.scale = layer1.scale
-		layer1.global_position = layer5.global_position
-		layer1.scale = layer5.scale
-		
-		layer5.global_position = buffer_position_4
-		layer5.scale = buffer_scale_4
-		
-		layer5.reparent(parallax_layer3)
-		layer4.reparent(parallax_layer2)
-		layer3.reparent(parallax_layer)
-		layer2.reparent(main_scene)
-		layer1.reparent(parallax_layer4)
-		
-		set_collision_layer_value(1,1)
-		set_collision_mask_value(1,1)
-		global_position.y-=100
-		
-		for i in DELTA:
-			animation.scale+= Vector2(0.15, 0.15)
-			await get_tree().create_timer(0.01).timeout
-		
-		await get_tree().create_timer(0.1).timeout
-		if is_on_floor():
-			animation.animation = "default" 
-		can_tp = 1
+	var layer1 = main_scene.get_child(3)
+	var layer2 = parallax_layer.get_child(0)
+	var layer3 = parallax_layer2.get_child(0)
+	var layer4 = parallax_layer3.get_child(0)
+	var layer5 = parallax_layer4.get_child(0)
+	
+	var buffer_position_4 = layer4.global_position
+	var buffer_scale_4 = layer4.scale
+	
+	layer4.global_position = layer3.global_position
+	layer4.scale = layer3.scale
+	layer3.global_position = layer2.global_position
+	layer3.scale = layer2.scale
+	layer2.global_position = layer1.global_position
+	layer2.scale = layer1.scale
+	layer1.global_position = layer5.global_position
+	layer1.scale = layer5.scale
+	
+	layer5.global_position = buffer_position_4
+	layer5.scale = buffer_scale_4
+	
+	layer5.reparent(parallax_layer3)
+	layer4.reparent(parallax_layer2)
+	layer3.reparent(parallax_layer)
+	layer2.reparent(main_scene)
+	layer1.reparent(parallax_layer4)
 
 func play_sfx(audio: AudioStreamOggVorbis, volume_intensity: float) -> void:
 	sfx_player.volume_db = volume_intensity
